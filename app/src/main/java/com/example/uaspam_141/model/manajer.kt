3a340e107425d7632b56473408e93aca4899fc71
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-22 21:51:08 +0700

    membuat model manajer

---
+++ app/src/main/java/com/example/uaspam_141/model/manajer.kt
@@ -0,0 +1,11 @@
+package com.example.uaspam_141.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class manajer(
+    @SerialName("id_manajer") val idmanajer: String,
+    @SerialName("nama_manajer") val namaManajer: String,
+    @SerialName("kontak_manajer") val kontakManajer: String
+)
