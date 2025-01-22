44dbc76e9148415c2c94d628951c31d8c74f663e
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-22 22:00:48 +0700

    membuat model properti dengan semua ketentuan dan foreign key dari 3 table

---
+++ app/src/main/java/com/example/uaspam_141/model/properti.kt
@@ -0,0 +1,18 @@
+package com.example.uaspam_141.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class properti(
+    @SerialName("id_properti") val idProperti: String,
+    @SerialName("nama_properti") val namaProperti: String,
+    @SerialName("deskripsi_properti") val deskripsiProperti: String,
+    @SerialName("lokasi") val lokasi: String,
+    @SerialName("harga") val harga: String,
+    @SerialName("status_properti") val statusProperti: String,
+
+    @SerialName("id_jenis") val idJenis: String,
+    @SerialName("id_pemilik") val idPemilik: String,
+    @SerialName("id_manajer") val idManajer: String,
+)
