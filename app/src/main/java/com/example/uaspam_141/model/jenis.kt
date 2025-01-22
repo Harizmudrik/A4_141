d9cc30765f062c098d0645d626ccb24027de620d
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-22 21:49:15 +0700

    membuat model "jenis" sesuai dengan ketentuan yang diberikan

---
+++ app/src/main/java/com/example/uaspam_141/model/jenis.kt
@@ -0,0 +1,12 @@
+package com.example.uaspam_141.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class jenis (
+    @SerialName("id_jenis") val idJenis: String,
+    @SerialName("nama_jenis") val namaJenis: String,
+    @SerialName("deskripsi_jenis") val deskripsi: String
+)
