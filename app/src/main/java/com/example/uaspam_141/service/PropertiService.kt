5f06c395772f9f5a153c4a0258305c676f8b72bd
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-22 23:21:41 +0700

    menambahkan properti

---
+++ app/src/main/java/com/example/uaspam_141/service/PropertiService.kt
@@ -0,0 +1,35 @@
+package com.example.uaspam_141.service
+
+import com.example.uaspam_141.model.Properti
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface PropertiService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    @GET("bacaproperti.php")
+    suspend fun getProperti(): List<Properti>
+
+    @GET("baca1properti.php/{id_properti}")
+    suspend fun getPropertiById(@Query("id_properti") idProperti: String): Properti
+
+    @POST("insertproperti.php")
+    suspend fun insertProperti(@Body properti: Properti): Response<Void>
+
+    @PUT("editproperti.php/{id_properti}")
+    suspend fun updateProperti(
+        @Query("id_properti") idProperti: String,
+        @Body properti: Properti
+    ): Response<Void>
+
+    @DELETE("deleteproperti.php/{id_properti}")
+    suspend fun deleteProperti(@Query("id_properti") idProperti: String): Response<Void>
+}
