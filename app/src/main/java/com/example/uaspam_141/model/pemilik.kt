0b158f0dad92c2b70e32da0e0e46f04a96db2d51
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-22 21:49:44 +0700

    membuat model "pemilik" seperti yang diminta

---
+++ app/src/main/java/com/example/uaspam_141/model/pemilik.kt
@@ -0,0 +1,11 @@
+package com.example.uaspam_141.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class pemilik(
+    @SerialName("id_pemilik") val idPemilik: String,
+    @SerialName("nama_pemilik") val namaPemilik: String,
+    @SerialName("kontak_pemilik") val kontakPemilik: String
+)
