95fb480b596377a1bb61a65f40e2b75f5ffd05ad
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-23 18:19:31 +0700

    membuat bagian untuk dapat menampilkan detail dalam view model nya pemilik

--- app/src/main/java/com/example/uaspam_141/ui/viewmodel/pemilik/DetailPemilikViewModel.kt
+++ app/src/main/java/com/example/uaspam_141/ui/viewmodel/pemilik/DetailPemilikViewModel.kt
@@ -1,2 +1,38 @@
 package com.example.uaspam_141.ui.viewmodel.pemilik
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.uaspam_141.model.Pemilik
+import com.example.uaspam_141.repository.PemilikRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class DetailPemilikUiState {
+    object Loading : DetailPemilikUiState()
+    data class Success(val pemilik: Pemilik) : DetailPemilikUiState()
+    object Error : DetailPemilikUiState()
+}
+
+class DetailPemilikViewModel(private val repository: PemilikRepository) : ViewModel() {
+    private val _uiState = MutableStateFlow<DetailPemilikUiState>(DetailPemilikUiState.Loading)
+    val uiState: StateFlow<DetailPemilikUiState> = _uiState
+
+    fun getPemilikById(idPemilik: String) {
+        viewModelScope.launch {
+            _uiState.value = DetailPemilikUiState.Loading
+            try {
+                val pemilik = repository.getPemilikById(idPemilik)
+                _uiState.value = DetailPemilikUiState.Success(pemilik)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                _uiState.value = DetailPemilikUiState.Error
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                _uiState.value = DetailPemilikUiState.Error
+            }
+        }
+    }
+}
