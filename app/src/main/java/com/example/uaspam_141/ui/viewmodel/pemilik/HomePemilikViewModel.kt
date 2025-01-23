a3bf6298c123d9b137a22558698429454a4567a3
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-23 18:20:50 +0700

    membuat bagian untuk dapat menampilkan halaman home dalam view model nya pemilik

--- app/src/main/java/com/example/uaspam_141/ui/viewmodel/pemilik/HomePemilikViewModel.kt
+++ app/src/main/java/com/example/uaspam_141/ui/viewmodel/pemilik/HomePemilikViewModel.kt
@@ -1,2 +1,52 @@
 package com.example.uaspam_141.ui.viewmodel.pemilik
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.uaspam_141.model.Pemilik
+import com.example.uaspam_141.repository.PemilikRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class HomePemilikUiState{
+    data class Success(val pemilik: List<Pemilik>): HomePemilikUiState()
+    object Error: HomePemilikUiState()
+    object Loading: HomePemilikUiState()
+}
+
+class HomePemilikViewModel (private val pemilikRepository: PemilikRepository): ViewModel(){
+    var pemilikUiState: HomePemilikUiState by mutableStateOf(HomePemilikUiState.Loading)
+        private set
+
+    init {
+        getPemilik()
+    }
+
+    fun getPemilik(){
+        viewModelScope.launch {
+            pemilikUiState = HomePemilikUiState.Loading
+            pemilikUiState = try {
+                HomePemilikUiState.Success(pemilikRepository.getPemilik())
+            }catch (e: IOException){
+                HomePemilikUiState.Error
+            }catch (e: HttpException){
+                HomePemilikUiState.Error
+            }
+        }
+    }
+
+    fun deletePemilik(idPemilik: String){
+        viewModelScope.launch {
+            try {
+                pemilikRepository.deletePemilik(idPemilik)
+            }catch (e: IOException){
+                HomePemilikUiState.Error
+            }catch (e: HttpException){
+                HomePemilikUiState.Error
+            }
+        }
+    }
+}
